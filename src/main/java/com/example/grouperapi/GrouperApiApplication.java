36bf25b6aa40635b55fee7d130566aebf186aa4e
Goshenkata <goshenkataklev@gmail.com>
2022-04-27 22:35:16 +0300

    boilerplate

---
+++ src/main/java/com/example/grouperapi/GrouperApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.grouperapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrouperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrouperApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grouperapi/GrouperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grouperapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrouperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
