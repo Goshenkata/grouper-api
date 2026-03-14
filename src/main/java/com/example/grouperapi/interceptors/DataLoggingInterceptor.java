dc05e557a4e0a8bfe29a902c97660349e1b5554a
Goshenkata <goshenkataklev@gmail.com>
2026-03-14 20:14:33 +0200

    Add preHandle method for user stats logging

--- src/main/java/com/example/grouperapi/interceptors/DataLoggingInterceptor.java
+++ src/main/java/com/example/grouperapi/interceptors/DataLoggingInterceptor.java
@@ -42,5 +42,24 @@ public class DataLoggingInterceptor implements HandlerInterceptor {
 		userStatsRepository.save(userStats);
 		return true;
 	}
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		String userAgent = request.getHeader("User-Agent");
+		Client client = parser.parse(userAgent);
+
+		UserStats userStats = new UserStats();
+		userStats.setMoment(Instant.now());
+		userStats.setUserType(request.getHeader("Authorization") == null ? UserType.ANONYMOUS : UserType.USER);
+		userStats.setBrowser(client.userAgent.family);
+		userStats.setPlatform(client.os.family);
+		log.debug(String.format("%s made request from %s running %s",
+				userStats.getUserType().name(),
+				userStats.getBrowser(),
+				userStats.getPlatform()
+		));
+		userStatsRepository.save(userStats);
+		return true;
+	}
 
 }
